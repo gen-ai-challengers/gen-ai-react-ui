b00b4aa93cf37a27dc1b0522043ef5b1f7bab659
haniissacmathew <hani7016@gmail.com>
2024-04-22 14:55:32 +0530

    babylonjs scene

--- src/catalogue/catalogueHome.jsx
+++ src/catalogue/catalogueHome.jsx
@@ -1,7 +1,13 @@
 import React, { useRef, useEffect } from 'react';
 import { Engine, Scene, FreeCamera, HemisphericLight, Mesh, StandardMaterial,Vector3 } from 'babylonjs';
-import '@babylonjs/loaders'; // Register loaders
 
+import '@babylonjs/loaders'; // Register loaders
+import "@babylonjs/core/Loading/loadingScreen";
+import "@babylonjs/loaders/glTF";
+import "@babylonjs/core/Materials/standardMaterial";
+import "@babylonjs/core/Materials/Textures/Loaders/envTextureLoader";
+import "@babylonjs/core/Animations/animatable"
+import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
 const CatelogueHome = () => {
     const canvasRef = useRef(null);
 
@@ -9,22 +15,25 @@ const CatelogueHome = () => {
         // Create Babylon.js engine and scene
         const engine = new Engine(canvasRef.current, true);
         const scene = new Scene(engine);
-
+        scene.onPointerDown = (evt) => {
+            if (evt.button === 0) engine.enterPointerlock();
+            if (evt.button === 1) engine.exitPointerlock();
+          };
+      
+          const framesPerSecond = 60;
+          const gravity = -9.81;
+          scene.gravity = new Vector3(0, gravity / framesPerSecond, 0);
+          scene.collisionsEnabled = true;
+        CreateEnvironment(scene);
         // ... (Add your Babylon.js logic here)
         const camera = new FreeCamera('camera1', new Vector3(10, 5, -10), scene);
         camera.setTarget(Vector3.Zero());
-        camera.attachControl(canvasRef.current, true);
+        camera.attachControl();
 
         // Create a hemispheric light
         const light = new HemisphericLight('light1', new Vector3(1, 1, 2), scene);
         light.intensity = 1;
 
-        // Create a sphere mesh
-        const sphere = Mesh.CreateSphere('sphere1', 16, 2, scene);
-
-        // Apply a standard material (optional)
-        const material = new StandardMaterial('material1', scene);
-        sphere.material = material;
         // Run the render loop
         engine.runRenderLoop(() => {
             scene.render();
@@ -35,9 +44,16 @@ const CatelogueHome = () => {
             engine.dispose();
         };
     }, []);
+    const CreateEnvironment=async(scene)=>{
+        const importPromise = SceneLoader.ImportMeshAsync(null, "./models/babylon/", "Prototype_Level.glb", scene);
+            importPromise.then((result) => {
+            //// Result has meshes, particleSystems, skeletons, animationGroups and transformNodes
+            });
+           
 
+    }
     return (
-        <canvas ref={canvasRef} style={{ width: '100%', height: '400px' }} />
+        <canvas ref={canvasRef} style={{ width: '100%', height: '600px' }} />
     );
 };
 
